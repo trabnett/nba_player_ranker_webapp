8b8c516288eada2387ff726776f1217c837fd964
trabnett <timrabnett@gmail.com>
2019-04-19 07:44:10 -0400

    first commit

---
+++ src/footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+      return (
+        <footer className="Header">
+            <h1>Thanks for visiting</h1>
+        </footer>
+      );
+    }
+  }
+  
+  export default Footer;
---
+++ src/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+      return (
+        <header className="Header">
+            <h1>Welcome to the NBA Player Ranker</h1>
+        </header>
+      );
+    }
+  }
+  
+  export default Header;
